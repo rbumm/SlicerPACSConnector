fe52a8c0a9540ef2bdf40d8c56c6143b8ef109aa
Rudolf Bumm <rudolf.bumm@googlemail.com>
2021-08-01 10:43:21 +0200

    Clinical test version

--- SlicerPACSConnector/SlicerPACSConnector.py
+++ SlicerPACSConnector/SlicerPACSConnector.py
@@ -491,8 +491,8 @@ class SlicerPACSConnectorLogic(ScriptedLoadableModuleLogic):
     startTime = time.time()
     logging.info('Processing started')
 
-    if not patientID:
-      raise ValueError("You need to specify a patient ID.")
+    if not patientID and not accessionNumber:
+      raise ValueError("You need to specify either a patientID or an accessionNumber.")
 
     if not callingAETitle:
       raise ValueError("callingAETitle missing.")
